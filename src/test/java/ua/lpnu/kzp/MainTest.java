4c9a746b8d3ed48cb5f48cfe217483b735ef2f34
Bohush Yura <bogusurij534@gmail.com>
2026-09-14 22:11:42 +0300

    реалізoвано інженерний рівень. closes #1

---
+++ src/test/java/ua/lpnu/kzp/MainTest.java
@@ -0,0 +1,11 @@
+package ua.lpnu.kzp;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+    @Test
+    void testApp() {
+        assertTrue(true, "Базовий тест проходить");
+    }
+}
